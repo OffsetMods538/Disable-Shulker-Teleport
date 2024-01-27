1435cfa0b0f2a8f7e102019cc1d5b0cd2622d583
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-01-27 12:43:59 +0200

    Mixin into 'tryTeleport' and disable teleporting

--- src/main/java/top/offsetmonkey538/disableshulkerteleport/mixin/ShulkerEntityMixin.java
+++ src/main/java/top/offsetmonkey538/disableshulkerteleport/mixin/ShulkerEntityMixin.java
@@ -2,7 +2,21 @@ package top.offsetmonkey538.disableshulkerteleport.mixin;
 
 import net.minecraft.entity.mob.ShulkerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShulkerEntity.class)
 public abstract class ShulkerEntityMixin {
+
+    @Inject(
+            method = "tryTeleport",
+            at = @At(
+                    value = "HEAD"
+            ),
+            cancellable = true
+    )
+    private void disableShulkerTeleport(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
 }
