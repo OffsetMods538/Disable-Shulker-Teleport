bd570d37220cc8ac69e57d47807a03c205bef3eb
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-01-27 12:40:51 +0200

    Add shulker entity mixin

---
+++ src/main/java/top/offsetmonkey538/disableshulkerteleport/mixin/ShulkerEntityMixin.java
@@ -0,0 +1,8 @@
+package top.offsetmonkey538.disableshulkerteleport.mixin;
+
+import net.minecraft.entity.mob.ShulkerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ShulkerEntity.class)
+public abstract class ShulkerEntityMixin {
+}
